d54678c79b1f83aae7a5c86a494fc6999e8b4c51
chicano79 <designs@microddl.net>
2024-03-09 08:57:12 +0000

    changes

--- Vivado_Projects/traffic_light/src/traffic_light.vhd
+++ Vivado_Projects/traffic_light/src/traffic_light.vhd
@@ -9,21 +9,24 @@ entity traffic_light is
 	
 	port(
 		clk_100MHz: in std_logic := '0';
-		--segment: out std_logic_vector(1 to 7) := (others => '1');
-		--ANODE: out std_logic_vector(1 to 8) := (0 => '0', others => '1');
+		segment: out std_logic_vector(1 to 7) := (others => '1');
+		ANODE: out std_logic_vector(1 to 4) := (1 => '0', others => '1');
+		LED: out std_logic := '0';
 		GPIO: out std_logic_vector(0 to 2) := (others => '0')	
 	);
 end entity;
 
 architecture rtl of traffic_light is
 
-type trafficLightStates is (stateA, stateB, stateC, stateD, stateE);
+type trafficLightStates is (initStart, stateA, stateB, stateC, stateD, stateE);
 
-signal trafficVariable: trafficLightStates := stateA;
+signal trafficVariable: trafficLightStates := initStart;
 
 signal redLED, yellowLED, greenLED: std_logic := '0';
 
 signal clk_2Hz, blink_en: std_logic := '0';
+
+signal bcd_counter: integer range 0 to 9 := 0; 
 
 begin
 
@@ -31,13 +34,15 @@ GPIO(2) <= redLED;
 GPIO(1) <= yellowLED xor (blink_en and clk_2Hz);
 GPIO(0) <= greenLED;
 
+LED <= clk_2Hz;
+
 TWO_HZ_GEN:
 	process(clk_100MHz)
-		constant count_range: integer range 0 to (FREQ/2)*2 := (FREQ/2)*2;
+		constant count_range: integer range 0 to FREQ/(2*2)  := FREQ/(2*2);
 		variable counter: integer range 0 to count_range;	
 	begin
 		if rising_edge(clk_100MHz) then
-			if counter < count_range then
+			if counter < (count_range-1) then
 				counter := counter + 1;
 			else
 				counter := 0;
@@ -45,76 +50,191 @@ TWO_HZ_GEN:
 			end if;
 		end if;
 	end process;
-
-
-TRAFFIC_STATES:
-	process(clk_100MHz)
+	
+DECODER_BLOCK:
+	with bcd_counter select
+		segment <=  "0000001" when 0,
+				    "1001111" when 1,
+				    "0010010" when 2,
+					"0000110" when 3,
+					"1001100" when 4,
+					"0100100" when 5,
+					"0100000" when 6,
+					"0001111" when 7,
+					"0000000" when 8,
+					"0000100" when 9,
+					"0000001" when others;
+	
+TF_SEGMENT_DISPLAY:
+	process(clk_100MHz)		
 		constant count_range_1sec: integer range 0 to FREQ   := FREQ*1;
-		constant count_range_2sec: integer range 0 to FREQ*2 := FREQ*2;
-		constant count_range_4sec: integer range 0 to FREQ*4 := FREQ*4;
-		constant count_range_5sec: integer range 0 to FREQ*5 := FREQ*5;
-		variable counter: integer range 0 to count_range_5sec;
+		variable counter: integer range 0 to FREQ;
+		variable counter2: integer range 0 to 10 := 0;
 	begin
 		if clk_100MHz'event and clk_100MHz = '1' then
 			case trafficVariable is
+				when initStart =>
+					bcd_counter <= 9;
+					counter2 := 0;
 			
-				when stateA => 
-					blink_en <= '0';
-					redLED <= '1';
-					yellowLED <= '0';
-					greenLED <= '0';					
-					if counter < count_range_5sec then 						
+				when stateA => 										
+					if counter < (count_range_1sec-1) then 						
+						counter := counter + 1;						
+					else
+						counter := 0;
+						bcd_counter <= bcd_counter - 1;
+						if counter2 < (9) then
+							counter2 := counter2 + 1;
+						else
+							counter2 := 0;
+							bcd_counter <= 5;					
+						end if;					
+					end if;
+						
+				when stateB => 					
+					if counter < (count_range_1sec-1) then 						
 						counter := counter + 1;
 					else
 						counter := 0;
+						bcd_counter <= bcd_counter - 1;
+						if counter2 < (5) then
+							counter2 := counter2 + 1;
+						else
+							counter2 := 0;
+							bcd_counter <= 9;					
+						end if;					
+					end if;
+			
+				when stateC =>					
+					if counter < (count_range_1sec-1) then 						
+						counter := counter + 1;
+					else
+						counter := 0;
+						bcd_counter <= bcd_counter - 1;
+						if counter2 < (9) then
+							counter2 := counter2 + 1;
+						else
+							counter2 := 0;
+							bcd_counter <= 2;					
+						end if;					
+					end if;			
+			
+				when stateD => 					
+					if counter < (count_range_1sec-1) then 						
+						counter := counter + 1;
+					else
+						counter := 0;
+						bcd_counter <= bcd_counter - 1;
+						if counter2 < (2) then
+							counter2 := counter2 + 1;
+						else
+							counter2 := 0;
+							bcd_counter <= 5;					
+						end if;					
+					end if;		
+						
+				when stateE =>					
+					if counter < (count_range_1sec-1) then 						
+						counter := counter + 1;
+					else
+						counter := 0;
+						bcd_counter <= bcd_counter - 1;
+						if counter2 < (5) then
+							counter2 := counter2 + 1;
+						else
+							counter2 := 0;
+							bcd_counter <= 9;					
+						end if;					
+					end if;		
+						
+				when others =>
+					bcd_counter <= 9;
+					counter2 := 0;			
+			end case;
+		end if;	
+	end process;
+	
+
+
+TRAFFIC_STATES:
+	process(clk_100MHz)		
+		--constant count_range_1sec: integer range 0 to FREQ*2 := FREQ*2;
+		constant count_range_2sec: integer range 0 to FREQ*3 := FREQ*3;
+		constant count_range_5sec: integer range 0 to FREQ*6 := FREQ*6;
+		constant count_range_9sec: integer range 0 to FREQ*10 := FREQ*10;		
+		variable counter: integer range 0 to count_range_9sec;
+	begin
+		if clk_100MHz'event and clk_100MHz = '1' then
+			case trafficVariable is
+				when initStart =>
+					blink_en <= '0';
+					redLED <= '1';
+					yellowLED <= '0';
+					greenLED <= '0';
+					trafficVariable <= stateA;
+			
+				when stateA => 										
+					if counter < (count_range_9sec-1) then 						
+						counter := counter + 1;
+					else
+						counter := 0;
+						
+						blink_en <= '0';
+						redLED <= '1';
+						yellowLED <= '1';
+						greenLED <= '0';
 						trafficVariable <= stateB;
 					end if;
 						
-				when stateB => 
-					blink_en <= '0';
-					redLED <= '1';
-					yellowLED <= '1';
-					greenLED <= '0';
-					if counter < count_range_2sec then 						
+				when stateB => 					
+					if counter < (count_range_5sec-1) then 						
 						counter := counter + 1;
 					else
 						counter := 0;
+						
+						blink_en <= '0';
+						redLED <= '0';
+						yellowLED <= '0';
+						greenLED <= '1';
 						trafficVariable <= stateC;
 					end if;
 			
-				when stateC =>
-					blink_en <= '0';
-					redLED <= '0';
-					yellowLED <= '0';
-					greenLED <= '1';
-					if counter < count_range_5sec then 						
+				when stateC =>					
+					if counter < (count_range_9sec-1) then 						
 						counter := counter + 1;
 					else
 						counter := 0;
+						
+						blink_en <= '0';
+						redLED <= '0';
+						yellowLED <= '1';
+						greenLED <= '1';
 						trafficVariable <= stateD;
 					end if;			
 			
-				when stateD => 
-					blink_en <= '0';
-					redLED <= '0';
-					yellowLED <= '1';
-					greenLED <= '1';
-					if counter < count_range_1sec then 						
+				when stateD => 					
+					if counter < (count_range_2sec-1) then 						
 						counter := counter + 1;
 					else
 						counter := 0;
+						
+						blink_en <= '1';
+						redLED <= '0';
+						yellowLED <= '1';
+						greenLED <= '1';
 						trafficVariable <= stateE;
 					end if;			
 						
-				when stateE => 
-					blink_en <= '1';
-					redLED <= '0';
-					yellowLED <= '1';
-					greenLED <= '1';
-					if counter < count_range_4sec then 						
+				when stateE =>					
+					if counter < (count_range_5sec-1) then 						
 						counter := counter + 1;
 					else
 						counter := 0;
+						
+						blink_en <= '0';
+						redLED <= '1';
+						yellowLED <= '0';
+						greenLED <= '0';
 						trafficVariable <= stateA;
 					end if;			
 						
