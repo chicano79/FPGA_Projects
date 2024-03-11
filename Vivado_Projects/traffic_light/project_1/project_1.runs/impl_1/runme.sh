2c2f6ff62854394472cc2c8be7e94ca1b8d3d26c
chicano79 <designs@microddl.net>
2024-03-11 09:42:33 +0000

    changes made

--- Vivado_Projects/traffic_light/project_1/project_1.runs/impl_1/runme.sh
+++ Vivado_Projects/traffic_light/project_1/project_1.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log traffic_light.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source traffic_light.tcl -notrace
 
 
--- Vivado_Projects/traffic_light/project_1/project_1.runs/impl_1/traffic_light.tcl
+++ Vivado_Projects/traffic_light/project_1/project_1.runs/impl_1/traffic_light.tcl
@@ -64,117 +64,15 @@ set_msg_config -id {Common 17-41} -limit 10000000
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 
-start_step init_design
-set ACTIVE_STEP init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param chipscope.maxJobs 2
-  set_param synth.incrementalSynthesisCache C:/Users/desig/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-18228-DESKTOP-7RQ9HLB/incrSyn
-  set_param xicom.use_bs_reader 1
-  create_project -in_memory -part xc7a35tcpg236-1
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir C:/Users/desig/Documents/GitHub/FPGA_Projects/Vivado_Projects/traffic_light/project_1/project_1.cache/wt [current_project]
-  set_property parent.project_path C:/Users/desig/Documents/GitHub/FPGA_Projects/Vivado_Projects/traffic_light/project_1/project_1.xpr [current_project]
-  set_property ip_output_repo C:/Users/desig/Documents/GitHub/FPGA_Projects/Vivado_Projects/traffic_light/project_1/project_1.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet C:/Users/desig/Documents/GitHub/FPGA_Projects/Vivado_Projects/traffic_light/project_1/project_1.runs/synth_1/traffic_light.dcp
-  read_xdc C:/Users/desig/Documents/GitHub/FPGA_Projects/Vivado_Projects/traffic_light/project_1/project_1.srcs/constrs_1/new/traffic_lights.xdc
-  link_design -top traffic_light -part xc7a35tcpg236-1
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force traffic_light_opt.dcp
-  create_report "impl_1_opt_report_drc_0" "report_drc -file traffic_light_drc_opted.rpt -pb traffic_light_drc_opted.pb -rpx traffic_light_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
-    implement_debug_core 
-  } 
-  place_design 
-  write_checkpoint -force traffic_light_placed.dcp
-  create_report "impl_1_place_report_io_0" "report_io -file traffic_light_io_placed.rpt"
-  create_report "impl_1_place_report_utilization_0" "report_utilization -file traffic_light_utilization_placed.rpt -pb traffic_light_utilization_placed.pb"
-  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file traffic_light_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step phys_opt_design
-set ACTIVE_STEP phys_opt_design
-set rc [catch {
-  create_msg_db phys_opt_design.pb
-  phys_opt_design 
-  write_checkpoint -force traffic_light_physopt.dcp
-  close_msg_db -file phys_opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed phys_opt_design
-  return -code error $RESULT
-} else {
-  end_step phys_opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force traffic_light_routed.dcp
-  create_report "impl_1_route_report_drc_0" "report_drc -file traffic_light_drc_routed.rpt -pb traffic_light_drc_routed.pb -rpx traffic_light_drc_routed.rpx"
-  create_report "impl_1_route_report_methodology_0" "report_methodology -file traffic_light_methodology_drc_routed.rpt -pb traffic_light_methodology_drc_routed.pb -rpx traffic_light_methodology_drc_routed.rpx"
-  create_report "impl_1_route_report_power_0" "report_power -file traffic_light_power_routed.rpt -pb traffic_light_power_summary_routed.pb -rpx traffic_light_power_routed.rpx"
-  create_report "impl_1_route_report_route_status_0" "report_route_status -file traffic_light_route_status.rpt -pb traffic_light_route_status.pb"
-  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file traffic_light_timing_summary_routed.rpt -pb traffic_light_timing_summary_routed.pb -rpx traffic_light_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file traffic_light_incremental_reuse_routed.rpt"
-  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file traffic_light_clock_utilization_routed.rpt"
-  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file traffic_light_bus_skew_routed.rpt -pb traffic_light_bus_skew_routed.pb -rpx traffic_light_bus_skew_routed.rpx"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force traffic_light_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-  unset ACTIVE_STEP 
-}
-
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param chipscope.maxJobs 2
+  set_param synth.incrementalSynthesisCache C:/Users/desig/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-18228-DESKTOP-7RQ9HLB/incrSyn
+  set_param xicom.use_bs_reader 1
+  open_checkpoint traffic_light_routed.dcp
+  set_property webtalk.parent_dir C:/Users/desig/Documents/GitHub/FPGA_Projects/Vivado_Projects/traffic_light/project_1/project_1.cache/wt [current_project]
   catch { write_mem_info -force traffic_light.mmi }
   write_bitstream -force traffic_light.bit 
   catch {write_debug_probes -quiet -force traffic_light}
